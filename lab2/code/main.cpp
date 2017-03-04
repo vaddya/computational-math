a6708a126824c6521a62d81873fe93d633e30e67
vaddya <vadik.dyachkov@gmail.com>
2017-03-04 21:10:54 +0300

    matrix code is readY

--- lab2/code/main.cpp
+++ lab2/code/main.cpp
@@ -2,33 +2,38 @@
 #include <c++/iostream>
 #include "matrix.h"
 
-#define type double
-
 using namespace std;
+using matrix::Matrix;
+using T = double;
+const int N = 8;
+const int M = 8;
 
+Matrix<T, N, M> findMatrixR(Matrix<T, N, M> matrix) {
+    return matrix * matrix::invert(matrix) - matrix::identity<T, N>();
+}
 
 int main() {
-    const int size = 8;
-    type p = 1;
-    type values[size][size] = {
-            {p + 13, 2,  8,  -7, 7,  5,  -7 - 7},
-            {7,      2,  -4, 2,  3,  3,  -1, -2},
-            {-7,     2,  1,  3,  6,  -6 - 3 - 4},
-            {-2,     -8, -6, -1, 6,  2,  1,  -4},
-            {0,      4,  -7, 1,  22, 0,  -6, -6},
-            {0,      -3, -6, 6,  4,  13, 0,  6},
-            {-8,     -6, -4, 7,  -5, -5, -2, 1},
-            {5,      5,  -2, -2, -3, 0,  -7, 14}
-    };
+    const T paramValues[] = {1, 0.1, 0.01, 0.001, 0.00001};
 
-    type *pointer[size];
-    for (int i = 0; i < size; ++i) {
-        pointer[i] = values[i];
+    for (auto p : paramValues) {
+        const T values[N][M] = {
+                {p + 13, 2,  8,  -7, 7,  5,  -7, -7},
+                {7,      2,  -4, 2,  3,  3,  -1, -2},
+                {-7,     2,  1,  3,  6,  -6, -3, -4},
+                {-2,     -8, -6, -1, 6,  2,  1,  -4},
+                {0,      4,  -7, 1,  22, 0,  -6, -6},
+                {0,      -3, -6, 6,  4,  13, 0,  6},
+                {-8,     -6, -4, 7,  -5, -5, -2, 1},
+                {5,      5,  -2, -2, -3, 0,  -7, 14}
+        };
+
+        Matrix<T, N, M> matrixA(values);
+        Matrix<T, N, M> matrixR = findMatrixR(matrixA);
+
+        cout << "p = " << p << endl
+             << "Matrix R: " << endl << matrixR
+             << "||R|| = " << matrixR.norm() << endl << endl;
     }
 
-    Matrix<type> matrix(size, size, pointer);
-    matrix[1][0] = 5;
-    cout << matrix << endl;
-    cout << matrix.norm() << endl;
     return 0;
 }
--- lab2/code/matrix.h
+++ lab2/code/matrix.h
@@ -3,119 +3,177 @@
 
 #include <c++/cstdlib>
 #include <c++/ostream>
+#include <c++/iomanip>
+#include "matrix_lib.h"
 
 using namespace std;
 
-template<typename T>
-class Matrix {
-public:
-    Matrix(int n) : Matrix(n, n) {}
+namespace matrix {
 
-    Matrix(int n, int m) {
-        this->n = n;
-        this->m = m;
-        values = new T *[n];
-        for (int i = 0; i < n; ++i) {
-            values[i] = new T[m];
-        }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                values[i][j] = 0;
+    template<typename T, size_t N, size_t M>
+    class Matrix {
+    public:
+        Matrix() {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < M; ++j) {
+                    values[i][j] = 0;
+                }
             }
         }
-    }
 
-    Matrix(int n, int m, T *values[]) {
-        this->n = n;
-        this->m = m;
-        this->values = new T *[n];
-        for (int i = 0; i < n; ++i) {
-            this->values[i] = new T[m];
-        }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                this->values[i][j] = values[i][j];
+        Matrix(const T values[N][M]) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < M; ++j) {
+                    this->values[i][j] = values[i][j];
+                }
             }
         }
-    }
 
-    T const *operator[](int i) const {
-        return &values[i][0];
-    }
+        Matrix(const Matrix<T, N, M> &rhs) : Matrix(rhs.values) {}
 
-    T *operator[](int i) {
-        return &values[i][0];
-    }
-
-    Matrix invert() {
-        return *this;
-    }
-
-    T norm() {
-        T max = 0.0;
-        for (int i = 0; i < n; i++) {
-            T sum = 0.0;
-            for (int j = 0; j < m; j++) {
-                sum += abs(values[i][j]);
+        Matrix<T, N, M> &operator=(const Matrix<T, N, M> &rhs) {
+            if (this != &rhs) {
+                memcpy(values, rhs.values, sizeof(values));
             }
-            max = sum > max ? sum : max;
+            return *this;
         }
-        return max;
-    }
 
-    Matrix &operator+(Matrix another) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                values[i][j] += another.values[i][j];
+        T const *operator[](int i) const {
+            return &values[i][0];
+        }
+
+        T *operator[](int i) {
+            return &values[i][0];
+        }
+
+        int rows() const {
+            return N;
+        }
+
+        int cols() const {
+            return M;
+        }
+
+        T norm() const {
+            T max = 0;
+            for (int i = 0; i < rows(); i++) {
+                T sum = 0;
+                for (int j = 0; j < cols(); j++) {
+                    sum += abs(values[i][j]);
+                }
+                max = sum > max ? sum : max;
+            }
+            return max;
+        }
+
+        Matrix<T, N, M> &operator+=(const Matrix<T, N, M> &rhs) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < M; ++j) {
+                    values[i][j] += rhs.values[i][j];
+                }
+            }
+            return *this;
+        }
+
+        Matrix<T, N, M> &operator-=(const Matrix<T, N, M> &rhs) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < M; ++j) {
+                    values[i][j] -= rhs.values[i][j];
+                }
+            }
+            return *this;
+        }
+
+        friend Matrix<T, N, M> operator+(Matrix<T, N, M> lhs, const Matrix<T, N, M> &rhs) {
+            lhs += rhs;
+            return lhs;
+        }
+
+        friend Matrix<T, N, M> operator-(Matrix<T, N, M> lhs, const Matrix<T, N, M> &rhs) {
+            lhs -= rhs;
+            return lhs;
+        }
+
+        friend ostream &operator<<(ostream &os, const Matrix<T, N, M> &matrix) {
+            os << setprecision(2);
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < M; ++j) {
+                    os << setw(11) << matrix[i][j];
+                }
+                os << '\n';
+            }
+            return os;
+        }
+
+        virtual ~Matrix() {}
+
+    private:
+        T values[N][M];
+
+        T abs(T value) const {
+            return value >= 0 ? value : -value;
+        }
+    };
+
+    template<typename T, size_t N1, size_t M1, size_t N2, size_t M2>
+    Matrix<T, N1, M2> operator*(Matrix<T, M1, N1> lhs, const Matrix<T, M2, N2> &rhs) {
+        if (M2 != N1) {
+            return Matrix<T, N1, M2>();
+        }
+        Matrix<T, N1, M2> res;
+        for (int i = 0; i < N1; i++) {
+            for (int j = 0; j < M2; j++) {
+                for (int k = 0; k < M1; k++) {
+                    res[i][j] += lhs[i][k] * rhs[k][j];
+                }
             }
         }
-        return *this;
+        return res;
     }
 
-    Matrix &operator-(Matrix another) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                values[i][j] -= another.values[i][j];
+    template<size_t N>
+    Matrix<double, N, N> invert(const Matrix<double, N, N> &matrix) {
+        double inverted[N][N];
+        double work[N];
+        double cond;
+        int ipvt[N];
+        double **lu = new double *[N];
+        for (int i = 0; i < N; ++i) {
+            lu[i] = new double[N];
+            for (int j = 0; j < N; ++j) {
+                lu[i][j] = matrix[i][j];
             }
         }
-        return *this;
-    }
-
-    Matrix &operator*(int k) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                values[i][j] *= k;
+        decomp(N, lu, &cond, ipvt, work);
+        for (int i = 0; i < N; i++) {
+            double vector[N];
+            for (int j = 0; j < N; j++) {
+                vector[j] = 0;
+            }
+            vector[i] = 1.0;
+            solve(N, lu, vector, ipvt);
+            for (int j = 0; j < N; j++) {
+                inverted[j][i] = vector[j];
+            }
+            vector[i] = 0;
+        }
+        Matrix<double, N, N> res;
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                res[i][j] = inverted[i][j];
             }
         }
-        return *this;
+        return res;
     }
 
-    ~Matrix() {
-        for (int i = 0; i < n; ++i) {
-            free(values[i]);
+    template<typename T, size_t N>
+    Matrix<T, N, N> identity() {
+        Matrix<T, N, N> res;
+        for (int i = 0; i < N; i++) {
+            res[i][i] = 1;
         }
-        free(values);
+        return res;
     }
 
-    T abs(T value) {
-        return value >= 0 ? value : -value;
-    }
-
-    friend ostream &operator<<(ostream &os, Matrix &matrix) {
-        for (int i = 0; i < matrix.n; ++i) {
-            for (int j = 0; j < matrix.m; ++j) {
-                os << matrix.values[i][j] << "\t";
-            }
-            os << '\n';
-        }
-        return os;
-    }
-
-private:
-    int n;
-    int m;
-    T **values;
-
-};
-
+}
 #endif // MATRIX_H
